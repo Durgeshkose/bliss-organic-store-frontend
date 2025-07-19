afc7969f5a165c9c5f6f23951fdcfe6dab16fee9
Durgeshkose <kosedurgesh@gmail.com>
2025-07-19 16:29:26 +0530

    Initial commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,14 +1,16 @@
 // src/utils/axiosInstance.js
 import axios from 'axios';
 
+const backendURL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',  // ✅ server base URL
-  withCredentials: true, // optional if using cookies
+  baseURL: backendURL,
+  withCredentials: true, // needed if you're using cookies
 });
 
 // Add a request interceptor to include the token in headers
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // or from AuthContext
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
